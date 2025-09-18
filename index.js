0629edc440ae78fd36e5811e1f240d14fb4330fe
garyyeh1 <67785749+garyyeh1@users.noreply.github.com>
2025-09-18 01:50:21 +0100

    Displaying data dynamically

--- index.js
+++ index.js
@@ -15,8 +15,34 @@ function addTask(e){
     let newTime = document.getElementById("time").value
 
     let newEntry = new CreateObject (newTask, newDate, newTime)
-    mylibrary.push(newEntry)
-    console.table(mylibrary)
-    let user = localStorage.setItem("task", JSON.stringify(newEntry));
-    console.log(user)
+    mylibrary.push(newEntry) //test
+    console.table(mylibrary) //test
+    
+    localStorage.setItem("task", JSON.stringify(newEntry));
+    let display = JSON.parse(localStorage.getItem("task"))
+
+    let entry = document.querySelector('#entry')
+    let entryDiv = document.createElement("div")
+    entryDiv.setAttribute("id",'task1')
+
+    //displaying entries dynamically
+    let data = Object.entries(display)
+    for(i = 0; i<data.length; i++){
+        let entryContent = document.createElement("p")
+        entryContent.setAttribute("class", data[i][0])
+        entryContent.textContent += data[i][1]
+        entryDiv.append(entryContent)
+    }
+    
+    
+    entry.append(entryDiv)
+
+
+    console.log(data[0][1])
+    
+    //why localStorage gets replaced everytime?
+    //when refresh the data should be saved
+
+
+    //need to use localStorage.key() later to loop through different task entries
 }
