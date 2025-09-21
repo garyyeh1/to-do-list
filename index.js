140b7bb03301eac1e7912d8deb8b95b59f36f798
garyyeh1 <67785749+garyyeh1@users.noreply.github.com>
2025-09-22 00:11:11 +0100

    Toggle Complete button

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class CreateObject {
         this.TASK = newTask;
         this.DATE = newDate;
         this.TIME = newTime;
+        this.COMPLETED = false;
     }
 }
 let mylibrary = JSON.parse(localStorage.getItem('task'))
@@ -33,28 +34,19 @@ function addTask(e){
     localStorage.setItem("task", JSON.stringify(mylibrary));
     let display = JSON.parse(localStorage.getItem("task"))
     // console.log(display)
-
     renderTask(display)
     reset()
 
     }   
 function renderTask(display){
     let entry = document.querySelector('#entry')
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 4d9782d74248b6c6184acaef72c5906f412a976d
     entry.innerHTML = ""; // prevents duplication
 
     //displaying entries dynamically
     for(let i = 0; i<display.length; i++){
         let entryDiv = document.createElement("div")
         entryDiv.setAttribute("class",'task')
-<<<<<<< HEAD
-
-=======
->>>>>>> 4d9782d74248b6c6184acaef72c5906f412a976d
         let data = Object.entries(display[i])
         data.forEach(([key,value])=>{
             let entryContent = document.createElement("p")
@@ -62,14 +54,19 @@ function renderTask(display){
             entryContent.textContent = value
             entryDiv.append(entryContent)
         })
-<<<<<<< HEAD
         
         let removeTask = document.createElement('button')
         removeTask.addEventListener("click", remove)
         removeTask.textContent = "Delete task"
         entryDiv.append(removeTask)
 
-        function remove(entryDiv){
+        let toggleButton = document.createElement("input")
+        toggleButton.setAttribute("type", "checkbox")
+        entryDiv.append(toggleButton)
+
+
+
+        function remove(){
         
             mylibrary.splice(i,1)
             localStorage.setItem("task", JSON.stringify(mylibrary));
@@ -88,13 +85,6 @@ function renderTask(display){
 
 
 
-=======
-        entry.append(entryDiv)
-    }
-}
-
-
->>>>>>> 4d9782d74248b6c6184acaef72c5906f412a976d
 //reset input fields
 function reset(){
     let taskFields = document.getElementById("task")
@@ -103,4 +93,14 @@ function reset(){
     taskFields.value =""
     dateFields.value =""
     timeFields.value =""
+}
+
+
+function toggleCompleted(){
+    let completed = document.getElementById("completed").value
+
+    if (completed.checked){
+        this.COMPLETED = true;
+        console.log("task completed")
+    }
 }
